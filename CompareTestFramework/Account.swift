0d3ecf7d48df9cb052677910c86c3142bfc78551
Shunya Yamashita <shymst125@gmail.com>
2017-07-23 02:54:29 +0900

    Accountのstructを追加

---
+++ CompareTestFramework/Account.swift
@@ -0,0 +1,17 @@
+//
+//  Account.swift
+//  CompareTestFramework
+//
+//  Created by Shunya Yamashita on 2017/07/23.
+//  Copyright © 2017年 shymst. All rights reserved.
+//
+
+import Accounts
+
+struct Account {
+    static var twitterAccount: ACAccount?
+}
+
+extension Account {
+    func loginToTwitter() {}
+}
